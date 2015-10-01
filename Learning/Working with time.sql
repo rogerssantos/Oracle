78dcb26559ddbbe2ec89e080c1f206b942577eea
Roger Santos <roger_santos@outlook.com>
2015-10-01 18:08:25 -0300

    New file

---
+++ Learning/Working with time.sql
@@ -0,0 +1,7 @@
+-- COMPLETE TIME
+SELECT SYSTIMESTAMP FROM DUAL;
+
+ -- HOUR
+select to_char(sysdate,'HH') from dual;
+select to_char(sysdate,'HH24') from dual;
+select to_char(sysdate,'HH12') from dual;
